81c0bcdb25938a88f34d7bde9a38aaa15336373e
priyanjanjb <priyanjanjb@gmail.com>
2025-03-08 14:17:03 +0530

    Read pdf and extract word from the PDF

--- frontEnd/autometedCVHandler.py
+++ frontEnd/autometedCVHandler.py
@@ -1,5 +1,8 @@
-#import the framework
+#import the library
 import streamlit as st
+from PyPDF2 import PdfReader
+
+
  
 
 st.title('Automated CV Handler')
@@ -18,5 +21,15 @@ if upload_cv:
 else: 
     st.write('File uploaded unsuccessfull {check the file type}')
 
+
+# read the pdf file
+if upload_cv is not None:
+    pdf_reader = PdfReader(upload_cv) # read your PDF file
+    # extract the text data from your PDF file after looping through its pages with the .extract_text() method
+    text_data= ""
+    for page in pdf_reader.pages: # for loop method
+        text_data+= page.extract_text()
+    
+    st.write(text_data) # view the text data
 
 submitButton = st.button('Submit')
